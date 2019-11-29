1d2eb7e7caed229eaec1d1c13749929c54219d95
Ciara Wharton <whartonciara1299@gmail.com>
2019-11-28 20:03:10 -0500

    Creating first subprogram

--- project3.s
+++ project3.s
@@ -13,3 +13,10 @@ main:
     la $a0,data // loading data
     li $a1, 1001 // limiting the amount of characters
     syscall // system call
+    jal subprogramOne // jumping to the first subprogram
+subprogramOne:
+sub $sp, $sp,4
+sw $a0, 0($sp)
+lw $t0, 0($sp) // loading input and storing it into register $t0
+addi $sp,$sp,4
+move $t6, $t0 // taking input and storing into register $t6
