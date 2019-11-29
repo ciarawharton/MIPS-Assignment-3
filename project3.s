c797208af88ec88b22f2d9913c04e082ba9a7d02
Ciara Wharton <whartonciara1299@gmail.com>
2019-11-28 19:31:11 -0500

    Starting
    Beginning

---
+++ project3.s
@@ -0,0 +1,9 @@
+//  project3.s
+//  Created by Ciara Wharton on 11/28/19.
+.data
+data: .space 1001 // limiting the amount of characters
+    output: .asciiz "\n"
+    notvalid: .asciiz "NaN"
+    comma: .asciiz ","
+.text
+main:
