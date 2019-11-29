f8f3c5dd06a15bba2c0adaf20572aab99b3fa7f6
Ciara Wharton <whartonciara1299@gmail.com>
2019-11-28 23:02:36 -0500

    Update project3.s
    Forgot that comment syntax is # not //
    :)

--- project3.s
+++ project3.s
@@ -1,71 +1,71 @@
-//  project3.s
-//  Created by Ciara Wharton on 11/28/19.
+#  project3.s
+#  Created by Ciara Wharton on 11/28/19.
 .data
-data: .space 1001 // limiting the amount of characters
+data: .space 1001 # limiting the amount of characters
     output: .asciiz "\n"
     notvalid: .asciiz "NaN"
     comma: .asciiz ","
 .text
 main:
 main:
-// takes in user input
+# takes in user input
     li $v0,8
-    la $a0,data // loading data
-    li $a1, 1001 // limiting the amount of characters
-    syscall // system call
-    jal subprogramOne // jumping to the first subprogram
-forward: // creating the moving forward function
-    j display // moving/jumping to the display function
+    la $a0,data # loading data
+    li $a1, 1001 # limiting the amount of characters
+    syscall # system call
+    jal subprogramOne # jumping to the first subprogram
+forward: # creating the moving forward function
+    j display # moving/jumping to the display function
 
 subprogramOne:
 sub $sp, $sp,4
 sw $a0, 0($sp)
-lw $t0, 0($sp) // loading input and storing it into register $t0
+lw $t0, 0($sp) # loading input and storing it into register $t0
 addi $sp,$sp,4
-move $t6, $t0 // taking input and storing into register $t6
+move $t6, $t0 # taking input and storing into register $t6
 
-beginning: // beginning function
+beginning: # beginning function
 li $t2,0
-li $t7, -1 // invalid input
+li $t7, -1 # invalid input
 lb $s0, ($t0)
-beq $s0, 9, skipCharacter // if the bit is a tab, it skips
-beq $s0, 32, skipCharacter // if the bit is a space, it skips
-move $t6, $t0 // storing characters
-j moving // moving to the looping function
-skipCharacter: // skipping characters
+beq $s0, 9, skipCharacter # if the bit is a tab, it skips
+beq $s0, 32, skipCharacter # if the bit is a space, it skips
+move $t6, $t0 # storing characters
+j moving # moving to the looping function
+skipCharacter: # skipping characters
     addi $t0,$t0,1
-    j beginning // jumping to the beginning function
-moving: // beginning of the loop
-    lb $s0, ($t0) // loading bits
-    beq $s0, 0, subCharacter // undefined bits
+    j beginning # jumping to the beginning function
+moving: # beginning of the loop
+    lb $s0, ($t0) # loading bits
+    beq $s0, 0, subCharacter # undefined bits
     beq $s0, 10, subCharacter
     addi $t0,$t0,1
-    beq $s0, 44, subCharacter // commas
+    beq $s0, 44, subCharacter # commas
 checkString:
-    bgt $t2,0,invalidCharacter // checking for spaces or tabs
-    beq $s0, 9,  skipping // if tab, move to gap function
-    beq $s0, 32, skipping // if space, move to gap function
-    ble $s0, 47, invalidCharacter // less than 48 = invalid
-    ble $s0, 57, vaildCharacter // integers = valid
-    ble $s0, 64, invalidCharacter // less than 64 = invalid
-    ble $s0, 84, vaildCharacter // starting with A (capital) = valid
-    beq $s0, 96, invalidCharacter // less than 96 = invalid
-    ble $s0, 117, vaildCharacter // less than 117 (u) = valid
-    beq $s0, 118, invalidCharacter // greater than 117 = valid
+    bgt $t2,0,invalidCharacter # checking for spaces or tabs
+    beq $s0, 9,  skipping # if tab, move to gap function
+    beq $s0, 32, skipping # if space, move to gap function
+    ble $s0, 47, invalidCharacter # less than 48 = invalid
+    ble $s0, 57, vaildCharacter # integers = valid
+    ble $s0, 64, invalidCharacter # less than 64 = invalid
+    ble $s0, 84, vaildCharacter # starting with A (capital) = valid
+    beq $s0, 96, invalidCharacter # less than 96 = invalid
+    ble $s0, 117, vaildCharacter # less than 117 (u) = valid
+    beq $s0, 118, invalidCharacter # greater than 117 = valid
 skipping:
     addi $t2,$t2,-1
-    j moving // jumping to loop function
+    j moving # jumping to loop function
 vaildCharacter:
     addi $t3, $t3,1
     mul $t2,$t2,$t7
-    j moving // jumping to loop function
+    j moving # jumping to loop function
 invalidCharacter:
-    lb $s0, ($t0) // loading bits
+    lb $s0, ($t0) # loading bits
     beq $s0, 0, withinString
     beq $s0, 10, withinString
     addi $t0,$t0,1
     beq $s0, 44, withinString
-    j invalidCharacter // jumping to function
+    j invalidCharacter # jumping to function
 withinString:
     addi $t1,$t1,1
     sub $sp, $sp,4
@@ -109,3 +109,28 @@ subprogram3:
     ble $s0, 57, number
     ble $s0, 84, uppercase
     ble $s0, 116, lowercase
+number:
+    sub $s0, $s0, 48
+    beq $t3, 0, combined
+    li $t9, 30
+    j exponent
+lowercase:
+    sub $s0, $s0, 87
+    beq $t3, 0, combined
+    li $t9, 30
+    j exponent
+uppercase:
+    sub $s0, $s0, 55
+    beq $t3, 0, combined
+    li $t9, 30
+    j exponent
+combined:
+    mul $s2, $t9, $s0
+    add $s1,$s1,$s2
+    j moveForward
+exponent:
+    ble $t8, 1, combined
+    mul $t9, $t9, 30
+    addi $t8, $t8, -1
+    j exponent
+
