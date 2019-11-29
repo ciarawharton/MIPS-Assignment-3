8ab49242c838212debcea1b73b8c01270b9f7dba
Ciara Wharton <whartonciara1299@gmail.com>
2019-11-28 20:20:49 -0500

    Creating the beginning function

--- project3.s
+++ project3.s
@@ -14,9 +14,21 @@ main:
     li $a1, 1001 // limiting the amount of characters
     syscall // system call
     jal subprogramOne // jumping to the first subprogram
+forward: // creating the moving forward function
+    j display // moving/jumping to the display function
+
 subprogramOne:
 sub $sp, $sp,4
 sw $a0, 0($sp)
 lw $t0, 0($sp) // loading input and storing it into register $t0
 addi $sp,$sp,4
 move $t6, $t0 // taking input and storing into register $t6
+
+beginning: // beginning function
+li $t2,0
+li $t7, -1 // invalid input
+lb $s0, ($t0)
+beq $s0, 9, skipCharacter // if the bit is a tab, it skips
+beq $s0, 32, skipCharacter // if the bit is a space, it skips
+move $t6, $t0 // storing characters
+j moving // moving to the looping function
